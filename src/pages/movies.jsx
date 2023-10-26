fe28126e36a24a53d21d78aad534111d5421f4fd
turko-yurii <turkoburko1993@gmail.com>
2023-10-26 19:53:22 +0300

    fix3

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -15,19 +15,6 @@ const ListOfFilm = styled.ul`
   }
 `;
 
-const InputWrapper = styled.form`
-  box-shadow: 0px 10px 10px rgba(46, 47, 66, 0.08),
-    0px 1px 1px rgba(46, 47, 66, 0.16), 0px 2px 1px rgba(46, 47, 66, 0.08);
-  padding: 10px;
-  border-radius: 5px;
-  background-color: #edf3fb;
-  input {
-    outline: none;
-  }
-  label {
-    color: grey;
-  }
-`;
 
 const Container = styled.section`
   padding: 20px;
